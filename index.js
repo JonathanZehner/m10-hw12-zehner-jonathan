b4d45dbdb040348a67beccc7f11b067ebb247633
Jonathan Zehner <jonathan.zehner@ufl.edu>
2021-12-11 12:26:43 -0500

    Create class Car and var named 'mySweetRide'

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // YOUR CODE HERE
+class Car {
+    constructor(make, model, year) {
+        this.make = 'Jeep';
+        this.model = 'Commander';
+        this.year = 2006;
+        this.honk();
+        this.performMaintenance()
+    }
+}
+
+const mySweetRide = new Car(
+    make, 'Pontiac',
+    model, 'Fiero',
+    year, 1988
+)
